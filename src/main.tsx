7f6f963e78b96e63447f962291a14e104865feb3
boss6704101335 <thanawat2549boss35@gmail.com>
2025-09-08 13:21:55 +0700

    n

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './Assignment/Styles.ts'
 //import TodoList from './component/TodoList.tsx'
-//import Course from './Assignment/Course.tsx'
+import Course from './Assignment/Course.tsx'
 //import TodoListHookFrom from './component/TodoListHookFrom.tsx'
 //import TodoListZod from './component/TodoListZod.tsx'
 import HookZod from './Assignment/HookZod.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <Course /> */}
+    <Course />
     <HookZod />
     {/* <TodoList /> */}
     {/* <TodoListHookFrom /> */}
